13a2b932febbbb532e3e5d64ddc7ced575ef744f
Your Name <nickmihaly@gmail.com>
2021-09-26 22:49:26 -0700

    npm run build

---
+++ client/build/precache-manifest.76ac9b74d57762bf8692da23e446c525.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe32f62ca78b3c61d8cf80a87ffab241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5cc6af110ed50aa5877",
+    "url": "/static/css/main.e213c893.chunk.css"
+  },
+  {
+    "revision": "141ffbf4d726e4e6ae0f",
+    "url": "/static/js/2.5b1669ec.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.5b1669ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5cc6af110ed50aa5877",
+    "url": "/static/js/main.4474b7e6.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
